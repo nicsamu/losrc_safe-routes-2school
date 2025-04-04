3fefbb5c2404f2d4546f099931ebd680bdee753f
nicsamu <nsamu9@gmail.com>
2025-04-04 16:08:42 -0400

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,14 +1,23 @@
-console.log("🔥 Firebase config loaded");
+function waitForFirebaseInit() {
+  if (typeof firebase !== "undefined" && firebase.initializeApp) {
+    console.log("🔥 Firebase available, initializing...");
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCC5bxMbUrzFVXI0mVUBXL4STHuNxfWODo",
-  authDomain: "school-route-safety-concerns.firebaseapp.com",
-  projectId: "school-route-safety-concerns",
-  storageBucket: "school-route-safety-concerns.appspot.com",
-  messagingSenderId: "753357994973",
-  appId: "1:753357994973:web:16d04af6a93e84dc465434",
-  measurementId: "G-BE0BNEQ6HD"
-};
+    const firebaseConfig = {
+      apiKey: "AIzaSyCC5bxMbUrzFVXI0mVUBXL4STHuNxfWODo",
+      authDomain: "school-route-safety-concerns.firebaseapp.com",
+      projectId: "school-route-safety-concerns",
+      storageBucket: "school-route-safety-concerns.appspot.com",
+      messagingSenderId: "753357994973",
+      appId: "1:753357994973:web:16d04af6a93e84dc465434",
+      measurementId: "G-BE0BNEQ6HD"
+    };
 
-firebase.initializeApp(firebaseConfig);
-window.db = firebase.firestore();
+    firebase.initializeApp(firebaseConfig);
+    window.db = firebase.firestore();
+  } else {
+    console.log("⏳ Waiting for Firebase...");
+    setTimeout(waitForFirebaseInit, 100);
+  }
+}
+
+waitForFirebaseInit();
